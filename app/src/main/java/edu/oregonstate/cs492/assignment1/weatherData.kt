27616aeee0c3e13ac4f1dbf6552befbf1cab14b2
Andrew <129807999+TheAndrewDang@users.noreply.github.com>
2024-02-06 17:53:38 -0800

    Fake Weather App

---
+++ app/src/main/java/edu/oregonstate/cs492/assignment1/weatherData.kt
@@ -0,0 +1,8 @@
+package edu.oregonstate.cs492.assignment1
+data class WeatherData(
+    var date: String,
+    var tempHigh: String,
+    var tempLow: String,
+    var probability: String,
+    var description: String,
+    var snackDescription: String)
